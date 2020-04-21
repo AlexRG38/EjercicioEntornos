2f2df3046850546d81c29960eaf3b6d413da30f7
FNMG2001 <paconieto14@gmail.com>
2020-04-21 18:40:10 +0200

    Test Case de TerminadosEnTres

---
+++ EjercicioEntornos/src/TerminadosEnTresTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class TerminadosEnTresTest {
+
+	@Test
+	public void testTerminadosTres() {
+		ArrayList<Integer> numero = new ArrayList<Integer>();
+		numero.add(0);
+		numero.add(0);
+		numero.add(0);
+		numero.add(0);
+		numero.add(0);
+
+		int resultado = TerminadosEnTres.verTerminaTres(numero);
+		assertEquals("El ArrayList es 0", resultado);
+	}
+}
