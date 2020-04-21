490d33f3dc05013a15bbc55ed623376230315bd7
Alejandro <alejandrorgg.2001@gmail.com>
2020-04-21 17:00:18 +0200

    Clase TerminadoEnTres Acabada

--- EjercicioEntornos/src/TerminadosEnTres.java
+++ EjercicioEntornos/src/TerminadosEnTres.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
 
 public class TerminadosEnTres {
 	public static void main(String [] args) {
 		
-	
+		ArrayList<Integer> numero = new ArrayList<Integer>();
+		numero.add(13);
+		numero.add(50);
+		numero.add(30);
+		numero.add(23);
+		numero.add(503);
+		System.out.println(verTerminaTres(numero));
 	}
+	
+	public static int verTerminaTres(ArrayList<Integer>numero) {
+		int contador = 0;
+		Object comprobar[] = numero.toArray();
+		for (int i = 0; i < comprobar.length; i++) {
+			if(String.valueOf(comprobar[i]).endsWith("3")) {
+				contador++;
+			}
+		}
+		return contador;
+	}
+	
 }
