61220f981b0c460d89e8bc39baa3ee81c4550eaa
Alejandro <alejandrorgg.2001@gmail.com>
2020-04-21 18:49:34 +0200

    Modificacion Junit Clase TerminadosEnTres

--- EjercicioEntornos/src/TerminadosEnTresTest.java
+++ EjercicioEntornos/src/TerminadosEnTresTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class TerminadosEnTresTest {
 
 	@Test
-	public void testTerminadosTres() {
+	public void testNumerosCero() {
 		ArrayList<Integer> numero = new ArrayList<Integer>();
 		numero.add(0);
 		numero.add(0);
@@ -17,5 +17,18 @@ class TerminadosEnTresTest {
 
 		int resultado = TerminadosEnTres.verTerminaTres(numero);
 		assertEquals(resultado,0);
+	}
+	
+	@Test
+	public void testProcedimientoNormal() {
+		ArrayList<Integer> numero = new ArrayList<Integer>();
+		numero.add(3);
+		numero.add(13);
+		numero.add(50);
+		numero.add(43);
+		numero.add(10);
+
+		int resultado = TerminadosEnTres.verTerminaTres(numero);
+		assertEquals(resultado,3);
 	}
 }
